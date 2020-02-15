250f6e665a28d918d756293a5ecccd694eb87d9d
stuoningur <stuoningur@gmail.com>
2020-02-15 14:44:31 +0100

    Initial commit of the base file.

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+client = discord.Client()
+
+load_dotenv()
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+client.run(os.getenv("BOT_TOKEN"))
