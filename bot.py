beb0b82949b2665c158246e86cae2eb24eb8dc76
stuoningur <stuoningur@gmail.com>
2020-02-15 16:47:02 +0100

    Switch to commands.Bot and add a shutdown command

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-client = discord.Client()
-
 load_dotenv()
+
+bot_token = os.getenv("BOT_TOKEN")
+owner_id = int(os.getenv("OWNER_ID"))
+bot_activity = os.getenv("BOT_ACTIVITY")
+
+client = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    activity=discord.Game(name=bot_activity),
+    case_insensitive=True,
+)
 
 
 @client.event
@@ -13,13 +22,12 @@ async def on_ready():
     print(f"We have logged in as {client.user}")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def shutdown(ctx):
+    """Shuts down the bot when the owner send the command"""
+    if ctx.message.author.id == owner_id:
+        print("Bot is shutting down")
+        await client.logout()
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
 
-
-client.run(os.getenv("BOT_TOKEN"))
+client.run(bot_token)
