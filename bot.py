a28a6b55a91dbea45ee1fd6714b11d751c039157
stuoningur <stuoningur@gmail.com>
2020-02-18 21:23:40 +0100

    Small changes to the owner check

--- bot.py
+++ bot.py
@@ -14,20 +14,21 @@ client = commands.Bot(
     command_prefix=commands.when_mentioned_or("!"),
     activity=discord.Game(name=bot_activity),
     case_insensitive=True,
+    owner_id=owner_id,
 )
 
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    print(f"Logged in as {client.user}")
 
 
 @client.command()
+@commands.is_owner()
 async def shutdown(ctx):
     """Shuts down the bot when the owner send the command"""
-    if ctx.message.author.id == owner_id:
-        print("Bot is shutting down")
-        await client.logout()
+    print(f"{client.user} is shutting down")
+    await client.logout()
 
 
 client.run(bot_token)
