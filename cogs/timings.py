181926009b6166cf60b3cfa6e4b1731889d25f56
stuoningur <50464069+stuoningur@users.noreply.github.com>
2020-07-24 20:47:29 +0200

    timings update

--- cogs/timings.py
+++ cogs/timings.py
@@ -3,39 +3,27 @@ from discord.ext import commands
 
 samsung_b_die_3600_lasch_spannung = "```VDIMM       1,35 - 1,38\nVDDG        0,95 - 1,05 (IOD & CCD)\nCLDO VDDP   855 - 1050\n```"
 samsung_b_die_3600_lasch_timings = "```tCL        16        tWRWRSCL     4\ntRCDRD     16 (17)   tRFC       288\ntRCDWR     16        tCWL        16\ntRP        16        tRTP        12\ntRAS       32        tRDWR       10\ntRC        42        tWRRD        4\ntRRDS      4         tWRWRSC      1\ntRRDL      6         tWRWRSD      7\ntFAW       24        tWRWRDD      7\ntWTRS      6         tRDRDSC      1\ntWTRL      12        tRDRDSD      5\ntWR        14        tRDRDDD      5\ntRDRDSCL   4         tCKE         1\n```"
-samsung_b_die_3600_lasch_rtts = (
-    "```2x8GB    0/0/5\n2x16GB   0/3/1\n4x8GB    7/3/1\n4x16GB   7/3/1```"
-)
+samsung_b_die_3600_lasch_rtts = "```2x8GB    0/0/5\n2x16GB   0/3/1 (Alternativ 0/2/1, 7/2/1 o. 5/2/1)\n4x8GB    7/3/1\n4x16GB   7/3/1```"
 
 samsung_b_die_3600_scharf_spannung = "```VDIMM       1,37 - 1,42\nVDDG        0,95 - 1,05 (IOD & CCD)\nCLDO VDDP   855 - 1050\n```"
 samsung_b_die_3600_scharf_timings = "```tCL        14        tWRWRSCL     4\ntRCDRD     14 (15)   tRFC       252\ntRCDWR     14        tCWL        14\ntRP        14        tRTP        12\ntRAS       28        tRDWR        8\ntRC        38        tWRRD        2\ntRRDS      4         tWRWRSC      1\ntRRDL      6         tWRWRSD      7\ntFAW       16        tWRWRDD      7\ntWTRS      4         tRDRDSC      1\ntWTRL      8         tRDRDSD      5\ntWR        14        tRDRDDD      5\ntRDRDSCL   4         tCKE         1\n```"
-samsung_b_die_3600_scharf_rtts = (
-    "```2x8GB    0/0/5\n2x16GB   0/3/1\n4x8GB    7/3/1\n4x16GB   7/3/1```"
-)
+samsung_b_die_3600_scharf_rtts = "```2x8GB    0/0/5\n2x16GB   0/3/1 (Alternativ 0/2/1, 7/2/1 o. 5/2/1)\n4x8GB    7/3/1\n4x16GB   7/3/1```"
 
 samsung_b_die_3733_lasch_spannung = "```VDIMM       1,37 - 1,42\nVDDG        0,95 - 1,05 (IOD & CCD)\nCLDO VDDP   855 - 1050\n```"
 samsung_b_die_3733_lasch_timings = "```tCL        16        tWRWRSCL     4\ntRCDRD     16 (17)   tRFC       299\ntRCDWR     16        tCWL        16\ntRP        16        tRTP        12\ntRAS       32        tRDWR       10\ntRC        42        tWRRD        4\ntRRDS      4         tWRWRSC      1\ntRRDL      6         tWRWRSD      7\ntFAW       24        tWRWRDD      7\ntWTRS      6         tRDRDSC      1\ntWTRL      12        tRDRDSD      5\ntWR        14        tRDRDDD      5\ntRDRDSCL   4         tCKE         1\n```"
-samsung_b_die_3733_lasch_rtts = (
-    "```2x8GB    0/0/5\n2x16GB   0/3/1\n4x8GB    7/3/1\n4x16GB   7/3/1```"
-)
+samsung_b_die_3733_lasch_rtts = "```2x8GB    0/0/5\n2x16GB   0/3/1 (Alternativ 0/2/1, 7/2/1 o. 5/2/1)\n4x8GB    7/3/1\n4x16GB   7/3/1```"
 
 samsung_b_die_3733_scharf_spannung = "```VDIMM       1,42 - 1,47\nVDDG        0,95 - 1,05 (IOD & CCD)\nCLDO VDDP   855 - 1050\n```"
 samsung_b_die_3733_scharf_timings = "```tCL        14          tWRWRSCL     4\ntRCDRD     15 (16)     tRFC       261\ntRCDWR     14          tCWL        14\ntRP        14          tRTP        12\ntRAS       28          tRDWR        8\ntRC        38 (42)     tWRRD        2\ntRRDS       4          tWRWRSC      1\ntRRDL       6          tWRWRSD      7\ntFAW       16          tWRWRDD      7\ntWTRS       4          tRDRDSC      1\ntWTRL       8          tRDRDSD      5\ntWR        14          tRDRDDD      5\ntRDRDSCL    4          tCKE         1\n```"
-samsung_b_die_3733_scharf_rtts = (
-    "```2x8GB    0/0/5\n2x16GB   0/3/1\n4x8GB    7/3/1\n4x16GB   7/3/1```"
-)
+samsung_b_die_3733_scharf_rtts = "```2x8GB    0/0/5\n2x16GB   0/3/1 (Alternativ 0/2/1, 7/2/1 o. 5/2/1)\n4x8GB    7/3/1\n4x16GB   7/3/1```"
 
 samsung_b_die_3800_lasch_spannung = "```VDIMM       1,40 - 1,45\nVDDG        0,95 - 1,05 (IOD & CCD)\nCLDO VDDP   855 - 1050\n```"
 samsung_b_die_3800_lasch_timings = "```tCL        16          tWRWRSCL     4\ntRCDRD     16 (17)     tRFC       304\ntRCDWR     16          tCWL        16\ntRP        16          tRTP        12\ntRAS       32          tRDWR       10\ntRC        42          tWRRD        4\ntRRDS       4          tWRWRSC      1\ntRRDL       6          tWRWRSD      7\ntFAW       24          tWRWRDD      7\ntWTRS       6          tRDRDSC      1\ntWTRL      12          tRDRDSD      5\ntWR        14          tRDRDDD      5\ntRDRDSCL    4          tCKE         1\n```"
-samsung_b_die_3800_lasch_rtts = (
-    "```2x8GB    0/0/5\n2x16GB   0/3/1\n4x8GB    7/3/1\n4x16GB   7/3/1```"
-)
+samsung_b_die_3800_lasch_rtts = "```2x8GB    0/0/5\n2x16GB   0/3/1 (Alternativ 0/2/1, 7/2/1 o. 5/2/1)\n4x8GB    7/3/1\n4x16GB   7/3/1```"
 
 samsung_b_die_3800_scharf_spannung = "```VDIMM       1,45 - 1,50\nVDDG        0,95 - 1,05 (IOD & CCD)\nCLDO VDDP   855 - 1050\n```"
 samsung_b_die_3800_scharf_timings = "```tCL        14          tWRWRSCL     4\ntRCDRD     15 (16)     tRFC       266\ntRCDWR     14          tCWL        14\ntRP        14          tRTP        12\ntRAS       28          tRDWR        8\ntRC        38 (42)     tWRRD        2 (4)\ntRRDS       4          tWRWRSC      1\ntRRDL       6          tWRWRSD      7\ntFAW       16          tWRWRDD      7\ntWTRS       4          tRDRDSC      1\ntWTRL       8          tRDRDSD      5\ntWR        14          tRDRDDD      5\ntRDRDSCL    4          tCKE         1\n```"
-samsung_b_die_3800_scharf_rtts = (
-    "```2x8GB    0/0/5\n2x16GB   0/3/1\n4x8GB    7/3/1\n4x16GB   7/3/1```"
-)
+samsung_b_die_3800_scharf_rtts = "```2x8GB    0/0/5\n2x16GB   0/3/1 (Alternativ 0/2/1, 7/2/1 o. 5/2/1)\n4x8GB    7/3/1\n4x16GB   7/3/1```"
 
 samsung_b_die_cads = "```24/20/20/24\n24/20/24/24\n24/24/24/24\n60/20/24/24```"
 
@@ -65,13 +53,13 @@ micron_e_die_3800_scharf_spannung = "```VDIMM       1,40\n```"
 micron_e_die_3800_scharf_timings = "```tCL        16   tWRWRSCL     4\ntRCDRD     19   tRFC       560\ntRCDWR     16   tCWL        16\ntRP        16   tRTP        12\ntRAS       38   tRDWR       10\ntRC        60   tWRRD        7\ntRRDS       6   tWRWRSC      1\ntRRDL       9   tWRWRSD      7\ntFAW       24   tWRWRDD      7\ntWTRS       4   tRDRDSC      1\ntWTRL      10   tRDRDSD      5\ntWR        16   tRDRDDD      5\ntRDRDSCL    4   tCKE         1\n```"
 micron_e_die_3800_scharf_rtts = "```2x8GB    0/0/5\n2x16GB   0/3/1 (Alternativ 7/3/1 o. 5/3/1)\n4x8GB    7/3/1\n4x16GB   7/3/1```"
 
-micron_e_die_cads = "```24/20/20/24```"
+micron_e_die_cads = "```24/24/24/24```"
 
 micron_e_die_3800_scharf_spannung_gdmoff = "```VDIMM       1,44\n```"
 micron_e_die_3800_scharf_timings_gdmoff = "```tCL        15   tWRWRSCL     4\ntRCDRD     18   tRFC       550\ntRCDWR     15   tCWL        14\ntRP        15   tRTP        12\ntRAS       32   tRDWR       10\ntRC        60   tWRRD        4\ntRRDS       4   tWRWRSC      1\ntRRDL       6   tWRWRSD      7\ntFAW       16   tWRWRDD      7\ntWTRS       5   tRDRDSC      1\ntWTRL      10   tRDRDSD      5\ntWR        16   tRDRDDD      5\ntRDRDSCL    4   tCKE         1\n```"
 micron_e_die_3800_scharf_rtts_gdmoff = "```7/3/1```"
 
-micron_e_die_gdmoff_cads = "```24/24/24/24```"
+micron_e_die_gdmoff_cads = "```24/20/20/24```"
 
 hynix_cjr_3600_spannung = "```VDIMM       1,35 - 1,37```"
 hynix_cjr_3600_timings = "```tCL        16   tWRWRSCL     4\ntRCDRD     18   tRFC       480\ntRCDWR     16   tCWL        16\ntRP        19   tRTP        12\ntRAS       38   tRDWR        9\ntRC        58   tWRRD        2\ntRRDS       6   tWRWRSC      1\ntRRDL       9   tWRWRSD      7\ntFAW       36   tWRWRDD      7\ntWTRS       4   tRDRDSC      1\ntWTRL      12   tRDRDSD      5\ntWR        12   tRDRDDD      5\ntRDRDSCL    4   tCKE         1\n```"
